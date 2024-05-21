a4abe8bcf4b244f5c7454e178d9986c795fda9f2
Sanskar Soni <sunscarsony@gmail.com>
2024-05-21 12:23:50 +0530

    Link tag created

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
-import './NavBar.css'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,23 +11,23 @@ const NavBar = () => {
 
   return (
     <div id="main-nav-content">
-    <nav className="navbar">
-      <div className="navbar-container">
-        <img src="/assets/psit.png" alt="logo" />
-        <div className={`navbar-links ${isOpen ? 'active' : ''}`}>
-          <a href="/" className="navbar-link">Home</a>
-          <a href="/about" className="navbar-link">About</a>
-          <a href="/services" className="navbar-link">Services</a>
-          <a href="/contact" className="navbar-link">Contact</a>
-          <a href="/teams" className="navbar-link">Team</a>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <img src="/assets/psit.png" alt="logo" />
+          <div className={`navbar-links ${isOpen ? 'active' : ''}`}>
+            <Link to="/" className="navbar-link">Home</Link>
+            <Link to="/about" className="navbar-link">About</Link>
+            <Link to="/services" className="navbar-link">Services</Link>
+            <Link to="/contact" className="navbar-link">Contact</Link>
+            <Link to="/teams" className="navbar-link">Team</Link>
+          </div>
+          <div id='btn-content'>
+            <button className="navbar-toggle" onClick={toggleNavbar}>
+              <span className="navbar-toggle-icon"></span>
+            </button>
+          </div>
         </div>
-        <div id='btn-content'>
-          <button className="navbar-toggle" onClick={toggleNavbar}>
-            <span className="navbar-toggle-icon"></span>
-          </button>
-        </div>
-      </div>
-    </nav>
+      </nav>
     </div>
   );
 };
